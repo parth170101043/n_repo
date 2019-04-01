b7780dcfd700311a963b9d39c9c348350b2e3698
parth170101043 <parthapratimm77@gmail.com>
2019-04-01 16:20:01 +0530

    curser

---
+++ WindowsApplication2/WindowsApplication2/Form1.Designer.vb
@@ -0,0 +1,202 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(198, 480)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(28, 17)
+        Me.Label13.TabIndex = 25
+        Me.Label13.Text = " gg"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(735, 382)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(28, 17)
+        Me.Label12.TabIndex = 24
+        Me.Label12.Text = " gg"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(636, 382)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(28, 17)
+        Me.Label11.TabIndex = 23
+        Me.Label11.Text = " gg"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(520, 382)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(28, 17)
+        Me.Label10.TabIndex = 22
+        Me.Label10.Text = " gg"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(393, 204)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(28, 17)
+        Me.Label9.TabIndex = 21
+        Me.Label9.Text = " gg"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(289, 195)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(28, 17)
+        Me.Label8.TabIndex = 20
+        Me.Label8.Text = " gg"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(162, 195)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(28, 17)
+        Me.Label7.TabIndex = 19
+        Me.Label7.Text = " gg"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(712, 195)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(28, 17)
+        Me.Label6.TabIndex = 18
+        Me.Label6.Text = " gg"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(636, 195)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(28, 17)
+        Me.Label5.TabIndex = 17
+        Me.Label5.Text = " gg"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(771, 118)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(28, 17)
+        Me.Label4.TabIndex = 16
+        Me.Label4.Text = " gg"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(412, 480)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(28, 17)
+        Me.Label3.TabIndex = 15
+        Me.Label3.Text = " gg"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(668, 589)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(16, 17)
+        Me.Label2.TabIndex = 14
+        Me.Label2.Text = "0"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(584, 589)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(16, 17)
+        Me.Label1.TabIndex = 13
+        Me.Label1.Text = "0"
+        '
+        'Timer1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(961, 725)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label13 As System.Windows.Forms.Label
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+
+End Class
---
+++ WindowsApplication2/WindowsApplication2/Form1.vb
@@ -0,0 +1,117 @@
+﻿Public Class Form1
+    Dim g As Graphics
+    Dim pt1 As Point
+    Dim pt2 As Point
+    Dim pt3 As Point
+    Dim pt4 As Point
+    Dim pt5 As Point
+    Dim ptctr As Point
+    Dim mypoly As Point()
+
+
+    ' Dim str As String = "C:\Users\IIT\Desktop\hmr1."
+    'Dim extension As String = Path.GetExtension(str)
+    Private Sub Form1_Paint(sender As Object, e As PaintEventArgs) Handles MyBase.Paint
+        
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Dim ms As New System.IO.MemoryStream(My.Resources.hmr1)
+        Dim ms As New System.IO.MemoryStream(My.Resources.oth441)
+        Me.Cursor = New Cursor(ms)
+        g = Me.CreateGraphics
+        Timer1.Start()
+
+    End Sub
+
+    Private Function cirpt(ByVal p1 As Point, ByVal p As Point, ByVal r As Double) As Integer
+        Dim cen As Point = New Point(p1.X + r / 2, p1.Y + r / 2)
+        Dim x As Double
+        Label7.Text = cen.X
+        Label8.Text = cen.Y
+
+        x = (cen.X - p.X) * (cen.X - p.X) + (cen.Y - p.Y) * (cen.Y - p.Y)
+        Label10.Text = CStr(cen.X - p.X)
+        Label11.Text = CStr(cen.Y - p.Y)
+        Label12.Text = CStr(r / 2)
+        Label9.Text = CStr(x)
+        If x < (r / 2) * (r / 2) Then
+            Return 0
+        Else
+            Return 1
+        End If
+    End Function
+
+    Private Function tript(ByVal p1 As Point, ByVal p2 As Point, ByVal p3 As Point, ByVal p As Point) As Integer
+        Dim arr As Double
+        arr = area(p1, p2, p3)
+        Dim ar1 As Double
+        ar1 = area(p, pt1, pt2)
+        Dim ar2 As Double
+        ar2 = area(p, pt2, pt3)
+        Dim ar3 As Double
+        ar3 = area(p, pt3, pt1)
+        Dim a As Double = ar1 + ar2 + ar3
+        If arr = a Then
+            Return 0
+        Else
+            Return 1
+        End If
+
+
+
+    End Function
+
+    Public Function area(ByVal p1 As Point, ByVal p2 As Point, ByVal p3 As Point) As Double
+        Dim ar As Double
+        ar = p1.X * (p2.Y - p3.Y) + p2.X * (p3.Y - p1.Y) + p3.X * (p1.Y - p2.Y)
+        If ar < 0 Then
+            ar = -1 * ar
+        End If
+        Return ar
+    End Function
+
+
+
+
+    Private Sub Form1_MouseClick(sender As Object, e As MouseEventArgs) Handles MyBase.MouseClick
+        Label1.Text = e.X
+        Label2.Text = e.Y
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        pt1 = New Point(1, 290)
+        pt2 = New Point(150, 290)
+        pt3 = New Point(81, 410)
+        mypoly = {pt1, pt2, pt3}
+        g.DrawPolygon(Pens.Black, mypoly)
+
+        Dim i As Integer = 230
+        Dim j As Integer = 290
+
+        ptctr = New Point(i, j)
+        g.DrawEllipse(Pens.Black, i, j, 150, 150)
+        pt4 = New Point(CInt(Label1.Text), CInt(Label2.Text))
+        Dim y As Integer = tript(pt1, pt2, pt3, pt4)
+        If y = 0 Then
+            Label13.Text = "yes"
+        Else
+            Label13.Text = ""
+        End If
+        Label5.Text = pt4.X
+        Label6.Text = pt4.Y
+        Dim x As Integer = cirpt(ptctr, pt4, 150)
+        Label4.Text = CStr(x)
+        If x = 0 Then
+            Label3.Text = "yes"
+        Else
+            Label3.Text = ""
+        End If
+
+    End Sub
+
+    
+    Private Sub Label7_Click(sender As Object, e As EventArgs) Handles Label7.Click
+       
+    End Sub
+End Class
